cac93e20003d61d7dcc2463ed99a7fea40fd63b4
Darah Via Moscoso <darahvia@Darahs-MacBook-Air.local>
2023-12-17 21:02:33 +0800

    FIXED THE ALTERNAING TASKS FINALLY I WANT TO CRY PLEASE THANK YOU UNIVERSEgit add UnscheduledEntryStrategy.javagit add UnscheduledEntryStrategy.java

--- UnscheduledEntryStrategy.java
+++ UnscheduledEntryStrategy.java
@@ -2,58 +2,58 @@ import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Queue;
-import java.util.Iterator;
 
 public class UnscheduledEntryStrategy {
     public void scheduleUnscheduledEntries(Queue<UnscheduledEntry> unscheduledEntriesQueue, EntryManager entryManager, TimeblockManager timeblockManager) {
 
-        
+        int c = 0;
+
         while (!unscheduledEntriesQueue.isEmpty()){
+            c++;
+            System.out.println("iteration " + c);
             List<Integer> availableSlots = timeblockManager.getAvailableSlots();
             UnscheduledEntry unscheduledEntry = unscheduledEntriesQueue.peek();
-            int units = unscheduledEntry.getUnits();
-
+            System.out.println(entryManager.getUnscheduledEntriesQueue());            
             int unitsPerTimeslot = unscheduledEntry.getUnitsPerTimeslot();
+            int timeslot = availableSlots.get(0) - 1;
+            int unitsRemaining = unscheduledEntry.getUnitsRemaining();
 
-            if (unitsPerTimeslot >= unscheduledEntry.getUnitsRemaining()){          // last iteration
-                int timeslot = availableSlots.get(0);
+            if (unitsPerTimeslot >= unitsRemaining){          // last iteration
+                System.out.println(unscheduledEntry.getName() + " last iteration");
                 LocalTime startTime = calculateTime(timeslot);
-                LocalTime endTime = calculateTime(timeslot + unscheduledEntry.getUnitsRemaining() * 4);     //duration
+                LocalTime endTime = calculateTime(timeslot + unitsRemaining * 4);     //duration
+                entryManager.addScheduledEntry(startTime, endTime, unscheduledEntry.getName());             // add to the allEntries queue
 
-                int startSlot = calculateMinutes(startTime);
+                int startSlot = calculateMinutes(startTime);                     // update occupied timeslots
                 int endSlot = calculateMinutes(endTime);
-
-                // to update the occupied timeslots
                 ArrayList<Integer> timeslotsToUpdate = new ArrayList<>();
                 for (int i = startSlot; i <= endSlot; i++) {
                     timeslotsToUpdate.add(i % timeblockManager.getTotalSlots());
                 }
-                timeblockManager.updateTimeslots(timeslotsToUpdate);
+                timeblockManager.updateTimeslots(timeslotsToUpdate);              
+                System.out.println("removed" + unscheduledEntriesQueue.poll().getName());         // no units remaining, time to dequeue
 
-                entryManager.addScheduledEntry(startTime, endTime, unscheduledEntry.getName());
-                unscheduledEntriesQueue.poll();
-                
+                continue;
+
             } else {
-                int timeslot = availableSlots.get(0);
+                System.out.println(unscheduledEntry.getName());
+
                 LocalTime startTime = calculateTime(timeslot);
                 LocalTime endTime = calculateTime(timeslot + unitsPerTimeslot * 4);     //duration
 
-                int startSlot = calculateMinutes(startTime);
+                int startSlot = calculateMinutes(startTime);                            // update occupied timeslots
                 int endSlot = calculateMinutes(endTime);
-                
-                
-                // to update the occupied timeslots
                 ArrayList<Integer> timeslotsToUpdate = new ArrayList<>();
                 for (int i = startSlot; i <= endSlot; i++) {
                     timeslotsToUpdate.add(i % timeblockManager.getTotalSlots());
                 }
                 timeblockManager.updateTimeslots(timeslotsToUpdate);
-
 
                 entryManager.addScheduledEntry(startTime, endTime, unscheduledEntry.getName());
                 unscheduledEntry.decreaseUnits(unitsPerTimeslot);
 
-                if (unscheduledEntry.getUnitsRemaining() > 0) {             // requeue
+                if (unitsRemaining > 0) {             // requeue
+                    unscheduledEntriesQueue.poll();
                     entryManager.getUnscheduledEntriesQueue().add(unscheduledEntry);
                 }
             }
