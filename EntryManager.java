a2f2fd7acfe51d591004d2028c92aba25a2e6294
Darah Via Moscoso <darahvia@Darahs-MacBook-Air.local>
2023-12-20 15:30:47 +0800

    add comments

--- EntryManager.java
+++ EntryManager.java
@@ -9,35 +9,46 @@ public class EntryManager{
     private Queue<UnscheduledEntry> unscheduledEntriesQueue;
     TimeblockManager timeblockManager = TimeblockManager.getInstance();
 
+    // constructor to initialize the data structure
     public EntryManager(){
         this.allEntries = new TreeMap<>();
         this.unscheduledEntriesQueue = new LinkedList<>();
     }
 
+    // returns a TreeMap containing all entries
     public TreeMap<Integer, CalendarEntry> getAllEntries(){
         return allEntries;
     }
 
+    // returns a Queue of unscheduled entries
     public Queue<UnscheduledEntry> getUnscheduledEntriesQueue(){
         return unscheduledEntriesQueue;
     }
 
+    // add a scheduled entry to the calendr
     public void addScheduledEntry(LocalTime startTime, LocalTime endTime, String taskName) { 
-        int startSlot = calculateMinutes(startTime);
-        int endSlot = calculateMinutes(endTime);
+        // calculate time slots foe start and end times
+        int startSlot = calculateSlot(startTime);
+        int endSlot = calculateSlot(endTime) - 1;
 
+        // add the scheduled entry to the TreeMap for each time slot in range
         allEntries.put(startSlot, new ScheduledEntry(taskName, startTime, endTime));
-        allEntries.put(endSlot, null);
-
+        for (int i = startSlot + 1; i <= endSlot; i++) {
+            allEntries.put(i, new ScheduledEntry(taskName, startTime, endTime));
+        }
+        
+        // update time block manager with the occupied time slots
         ArrayList<Integer> timeslotsToUpdate = new ArrayList<>();
         for (int i = startSlot; i <= endSlot; i++) {
             timeslotsToUpdate.add(i);
         }
 
         timeblockManager.updateTimeslots(timeslotsToUpdate);
+        System.out.println(timeblockManager.getAvailableSlots());        
     }
 
-    public int calculateMinutes (LocalTime time) {
-        return time.getHour() * 4 + time.getMinute() / 15;  //returns the numeric value of the timeslot within 0 to 95
+    //returns the numeric value of the timeslot within 0 to 95
+    public int calculateSlot (LocalTime time) {
+        return time.getHour() * 4 + time.getMinute() / 15;  
     }
 }
