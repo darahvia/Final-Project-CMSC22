9fb6263c0ad13210ca85d4c008f60ee0f63f0fa3
Darah Via Moscoso <darahvia@Darahs-MacBook-Air.local>
2023-12-20 15:27:07 +0800

    add comments

--- ScheduledEntry.java
+++ ScheduledEntry.java
@@ -1,13 +1,22 @@
 import java.time.LocalTime;
 
+/**
+ * represents the scheduled entry in a calendar
+ */
 public class ScheduledEntry extends CalendarEntry {
     private String name;
     private LocalTime startTime;
     private LocalTime endTime;
 
-
+/**
+ * constructor for creating a scheduled entry
+ * 
+ * @param name          name of scheduled entry
+ * @param startTime     start time of scheduled entry
+ * @param endTime       end time of scheduled entry
+ */
     public ScheduledEntry(String name, LocalTime startTime, LocalTime endTime) {
-        super(name);
+        super(name);            // call the constructor of the super class CalendarEntry
         this.name = name;
         this.startTime = startTime;
         this.endTime = endTime;
